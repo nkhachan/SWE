428d63fb3d38d23d05adcd63eea6dd1816b976d3
nkhachan <noopur.khachane@gmail.com>
2018-02-10 08:29:55 -0800

    Fixed arduino

--- Activity 2: EMG and Arduino/EMG/EMG.ino
+++ Activity 2: EMG and Arduino/EMG/EMG.ino
@@ -1,17 +1,11 @@
-#include "CurieIMU.h"
-#include "CurieTimerOne.h"
 void setup() {
  Serial.begin(115200);   //Initialize Serial Communication
- while(!Serial);     //Wait for connection
- CurieIMU.begin();
- CurieTimerOne.start(5000,EMG); //Create an interrupt function for taking EMG sample
  }
  
 void loop() {
-}
-void EMG(){   //Interrupt for taking EMG sample
    Serial.print(micros());  //print out time in seconds
    Serial.print("\t");
    Serial.println(analogRead(A0));  //Print out the raw EMG values into serial monitor
+   delay(200);
 }
 
